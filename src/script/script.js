37cb7b0658debef96e4907846a2bc890f2f09485
rftal rft <rftal1@fiema.org.br>
2025-07-10 20:59:10 -0300

    todos os arquivos da branch MenuMobile sendo modificados devido a problemas tecnicos emergenciais

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,7 @@
 import initTab from "./modules/tabNavigation.js";
 import initAccordion from "./modules/accordion.js";
-
+import initMenuMobile from "./modules/menuMobile.js";
 initTab();
 initAccordion();
+initMenuMobile();
 
