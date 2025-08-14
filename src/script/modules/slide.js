23751357dcfcb2f853d63f2e05914e7d389d685f
rftal rft <rftal1@fiema.org.br>
2025-08-13 21:12:17 -0300

    slide temporario

---
+++ src/script/modules/slide.js
@@ -0,0 +1,30 @@
+export default function initSlide(){
+
+    let slideAtivo=0;
+    const slides=document.querySelector('[data-slides="slides"]');
+    const dots=document.querySelectorAll('[data-dots="dots"] div');
+    const prevButton=document.querySelector('.arrow.preview');
+    const nextButton=document.querySelector('.arrow.next');
+
+   prevButton.addEventListener('click',()=> mudarSlide(-1));
+   nextButton.addEventListener('click',()=> mudarSlide(1));
+
+   function updateSlide(){
+    slides.style.transform=`translateX(-${mudarSlide*100})`
+    dots.forEach((itemDot, indice)=>{
+        itemDot.classList.toggle('ativo', indice === slideAtivo);
+    })
+   }
+
+
+   function mudarSlide(direction){
+    const total=document.querySelectorAll('[data-slides="slides"] div').length;
+    slideAtivo=(slideAtivo+ direction+total)% total;
+    updateSlide();
+
+
+   }
+   
+}
+
+
--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,7 @@
 import initTab from "./modules/tabNavigation.js";
 import initAccordion from "./modules/accordion.js";
-
+import initSlide from "./modules/slide.js";
 initTab();
 initAccordion();
+initSlide();
 
